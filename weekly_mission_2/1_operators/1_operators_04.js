ce53d3234156ac32d559ea139e87ab589e0f37ca
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:43:06 -0500

    Create 1_operators_04.js

---
+++ weekly_mission_2/1_operators/1_operators_04.js
@@ -0,0 +1,8 @@
+// Example 5: map() to convert to upper case the elements on a list
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const namesToUpperCase = names.map((name) => name.toUpperCase());
+
+console.log(
+  "Example 5: map to convert to upper case the elements on a list"
+);
+console.log(namesToUpperCase);
