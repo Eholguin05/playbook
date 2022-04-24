6a52959560b06cccbf0507294ee69fa4d8504622
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:25:48 -0500

    Create 0_objects_3.js

---
+++ weekly_mission_2/0_objects/0_objects_3.js
@@ -0,0 +1,12 @@
+// Example 4: Object with methods
+const pet = {
+  name: "Mimis",
+  // This function is stored as property
+  sayHello: function () {
+    // this.name refers to the object property
+    console.log(`${this.name} greets you!`);
+  },
+};
+
+console.log("Example 4: Object with methods");
+pet.sayHello();
