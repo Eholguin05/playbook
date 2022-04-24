fc34aa4d4b8adddd6e96e39e85da9cb897854671
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:16:22 -0500

    Create 0_objects_1.js

---
+++ weekly_mission_2/0_objects_1.js
@@ -0,0 +1,7 @@
+// Example 2: Creation of an object with properties
+const myObjetc2 = {
+  name: "Emmanuel",
+  age: 32,
+};
+console.log("Example 2: Creation of an object with properties");
+console.log(myObjetc2);
