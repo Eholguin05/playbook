2f6e88cfd771607c0402ba26dfc74070af79dcd4
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:18:51 -0500

    Delete 0_objects_1.js

--- weekly_mission_2/0_objects_1.js
+++
@@ -1,7 +0,0 @@
-// Example 2: Creation of an object with properties
-const myObjetc2 = {
-  name: "Emmanuel",
-  age: 32,
-};
-console.log("Example 2: Creation of an object with properties");
-console.log(myObjetc2);
