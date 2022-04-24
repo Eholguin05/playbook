124a52a61be2ffc65ebef128da8732df282b3b4d
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:53:28 -0500

    Create 1_operators_14.js

---
+++ weekly_mission_2/1_operators/1_operators_14.js
@@ -0,0 +1,4 @@
+//Example 15: sort() sorts the elements of an array
+const products = ["Milk", "Coffee", "Sugar", "Honey", "Apple", "Carrot"];
+console.log("Example 15: sorted elements");
+console.log(products.sort());
