0904a382ecd608f06e8f1ab2533e65d6f8d2af10
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:40:38 -0500

    Create 1_operators_00.js

---
+++ weekly_mission_2/1_operators/1_operators_00.js
@@ -0,0 +1,4 @@
+// Example 1: forEach() to print elements from a list
+const numbers = [1, 2, 3, 4, 5];
+console.log("Example 1: forEach to print elements from a list");
+numbers.forEach(console.log);
