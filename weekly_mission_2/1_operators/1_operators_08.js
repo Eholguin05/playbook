70f2a598183311c85062f3180bf24fce8ba3dfa1
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:46:40 -0500

    Create 1_operators_08.js

---
+++ weekly_mission_2/1_operators/1_operators_08.js
@@ -0,0 +1,5 @@
+// Example 9: reduce()
+const numbers = [1, 2, 3, 4, 5];
+const resultOfReduce = numbers.reduce((acc, element) => acc + element, 0);
+console.log("Example 9: reduce to calculate the sum of the elements in a list");
+console.log(resultOfReduce);
