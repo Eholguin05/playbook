5c3b1a3ae4916969d714fe73720dc3abfc3d7cd7
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:41:23 -0500

    Create 1_operators_01.js

---
+++ weekly_mission_2/1_operators/1_operators_01.js
@@ -0,0 +1,8 @@
+// Example 2: forEach() to calculate the sum of each element from a list
+let sum = 0;
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach((num) => (sum += num));
+console.log(
+  "Example 2: forEach to calculate the sum of each element from a list"
+);
+console.log(sum);
