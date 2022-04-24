17cc20d4402aefcc38138539ff2771b1bcbc2de2
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:24:55 -0500

    Create 0_objects_2.js

---
+++ weekly_mission_2/0_objects/0_objects_2.js
@@ -0,0 +1,15 @@
+// Example 3: Object with different properties
+const myObject3 = {
+  name: "Alejandro",
+  age: 12,
+  nicknames: ["Ale", "Alex"],
+  address: {
+    zip_code: "85130",
+    street: "Av. Jesús García",
+    city: "Cd. Obregón",
+  },
+};
+console.log("Example 3: Object with different properties");
+console.log(myObject3);
+console.log(myObject3.name);
+console.log(myObject3["address"]);
