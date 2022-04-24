21895a16f102b43549d5aaeb1acbfc5edccb748d
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:42:30 -0500

    Create 1_operators_03.js

---
+++ weekly_mission_2/1_operators/1_operators_03.js
@@ -0,0 +1,14 @@
+
+// Example 4: map() to get every single element from a list and create a new one
+/*
+  Arrow function and explicit return
+  const modifiedArray = arr.map((element,index) => element);
+*/
+const numbers = [1, 2, 3, 4, 5];
+const numbersSquare = numbers.map(function (num) {
+  return num * num;
+});
+// The function can also be writtern in fat arrow
+// const numbersSquare = numbers4.map((num) => return num * num)
+console.log("Example 4: Printing the list of square elements");
+console.log(numbersSquare);
