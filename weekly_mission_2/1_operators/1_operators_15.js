5b20794d56bd2a4ddd69212e235e058809de4033
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:53:48 -0500

    Create 1_operators_15.js

---
+++ weekly_mission_2/1_operators/1_operators_15.js
@@ -0,0 +1,18 @@
+
+// Example 16: sort() to sort a list of objects
+const users = [
+  { name: "A", age: 150 },
+  { name: "B", age: 50 },
+  { name: "C", age: 100 },
+  { name: "D", age: 22 },
+];
+
+users.sort((a, b) => {
+  // Using a function to sort the elements
+  if (a.age < b.age) return -1;
+  if (a.age > b.age) return 1;
+  return 0;
+});
+
+console.log("Example 16: Sorting the list of objects by the age");
+console.log(users); // sorted ascending
