8c7db0fcd9ea5a2c38f714e79ca6eceb2f886b3d
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:48:06 -0500

    Create 1_operators_10.js

---
+++ weekly_mission_2/1_operators/1_operators_10.js
@@ -0,0 +1,4 @@
+// Example 11: find() to get the first element of a list that pass the test implemented
+const ages = [24, 22, 19, 25, 32, 35, 18];
+const age = ages.find((age) => age < 20);
+console.log("Example 11: Which is the first age under 20? " + age);
