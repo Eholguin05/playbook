662f83d0fd5c8253bd794d80c87f058455607184
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-04-23 23:41:51 -0500

    Create 1_operators_02.js

---
+++ weekly_mission_2/1_operators/1_operators_02.js
@@ -0,0 +1,4 @@
+// Example 3: forEach() to print the countries in upper case
+const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+console.log("Example 3: forEach to print the countries in upper case");
+countries.forEach((element) => console.log(element.toUpperCase()));
